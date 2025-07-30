ae1b17cfda990fd4a308ea47ffb4818291659c15
bogfanof <sano.tsarevich@gmail.com>
2025-07-30 17:02:12 +0300

    car-rent commit 6 add login screen

---
+++ app/src/main/java/com/example/composecarrent/ui/theme/login_screen/LoginScreen.kt
@@ -0,0 +1,97 @@
+package com.example.composecarrent.ui.theme.login_screen
+
+import android.graphics.drawable.shapes.Shape
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composecarrent.R
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreen() {
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Column() {
+            Image(
+                modifier = Modifier.fillMaxWidth(),
+                painter = painterResource(id = R.drawable.im_car_rent_logo),
+                contentDescription = "Car_Rent_Logo",
+                alignment = Alignment.BottomCenter
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(15.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TextField(
+                    value = email,
+                    onValueChange = { email = it },
+                    label = { Text("You email") },
+                    singleLine = true,
+                    shape = RoundedCornerShape(10.dp)
+                )
+                TextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    label = { Text("You password") },
+                    singleLine = true,
+                    shape = RoundedCornerShape(10.dp)
+                )
+                Spacer(modifier = Modifier.height(150.dp))
+                Button(
+                    onClick = {
+
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth(0.7f)
+                        .height(100.dp),
+                    colors = ButtonDefaults.buttonColors(Color.White),
+                    border = ButtonDefaults.outlinedButtonBorder
+                )
+                {
+                    Text(
+                        text = "Login",
+                        color = Color.Black
+                    )
+                }
+            }
+        }
+    }
+}
